bfebc93be4a97d8dec0667bfe3097ee0d9f6ae32
ofreax <bertrand.gouny@gmail.com>
2015-11-08 13:31:26 +0100

    better ssl

--- image/service/postfix/container-start.sh
+++ image/service/postfix/container-start.sh
@@ -12,10 +12,13 @@ if [ ! -e "$FIRST_START_DONE" ]; then
   # check certificat and key or create it
   /sbin/ssl-helper "/container/service/postfix/assets/certs/$MMC_MAIL_SSL_CRT_FILENAME" "/container/service/postfix/assets/certs/$MMC_MAIL_SSL_KEY_FILENAME" --ca-crt=/container/service/postfix/assets/certs/$MMC_MAIL_SSL_CA_CRT_FILENAME
 
-  # adapt tls ldif
+  # adapt tls config
   sed -i "s|{{ MMC_MAIL_SSL_CA_CRT_FILENAME }}|${MMC_MAIL_SSL_CA_CRT_FILENAME}|g" /etc/postfix/main.cf
   sed -i "s|{{ MMC_MAIL_SSL_CRT_FILENAME }}|${MMC_MAIL_SSL_CRT_FILENAME}|g" /etc/postfix/main.cf
   sed -i "s|{{ MMC_MAIL_SSL_KEY_FILENAME }}|${MMC_MAIL_SSL_KEY_FILENAME}|g" /etc/postfix/main.cf
+
+  sed -i "s|{{ MMC_MAIL_SSL_CA_DHPARAM_1024 }}|${MMC_MAIL_SSL_CA_DHPARAM_1024}|g" /etc/postfix/main.cf
+  sed -i "s|{{ MMC_MAIL_SSL_CA_DHPARAM_512 }}|${MMC_MAIL_SSL_CA_DHPARAM_512}|g" /etc/postfix/main.cf
 
   # ldap config
   for i in `ls /etc/postfix/ldap-*.cf`;
@@ -50,6 +53,13 @@ if [ ! -e "$FIRST_START_DONE" ]; then
       echo "tls_key = /container/service/postfix/assets/ldap-client/certs/$MMC_MAIL_LDAP_CLIENT_TLS_KEY_FILENAME" >> $i;
     fi
   done
+
+  # prevent fatal: unknown service: smtp/tcp
+  # http://serverfault.com/questions/655116/postfix-fails-to-send-mail-with-fatal-unknown-service-smtp-tcp
+  cp -f /etc/services /var/spool/postfix/etc/services
+
+  # copy dns settings to chroot jail
+  cp -f /etc/resolv.conf /var/spool/postfix/etc/resolv.conf
 
   touch $FIRST_START_DONE
 fi
