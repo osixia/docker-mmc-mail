a835dc592176f02215c0fd7c0bcbdc5e01402a09
Bertrand Gouny <bgouny@hipay.com>
2015-12-16 09:50:30 +0100

    sed -i --follow-symlinks

--- image/service/dovecot/assets/config/config-available/replication/enable.sh
+++ image/service/dovecot/assets/config/config-available/replication/enable.sh
@@ -1,14 +1,14 @@
 #!/bin/bash -e
 
 ln -s /container/service/dovecot/assets/config/config-available/replication/config/90-replication.conf /etc/dovecot/conf.d/90-replication.conf
-sed -i "s|mail_plugins = \$mail_plugins|mail_plugins = \$mail_plugins notify replication|g" /etc/dovecot/conf.d/10-mail.conf
+sed -i --follow-symlinks "s|mail_plugins = \$mail_plugins|mail_plugins = \$mail_plugins notify replication|g" /etc/dovecot/conf.d/10-mail.conf
 
-sed -i "s|{{ MMC_MAIL_REPLICATION_PASSWORD }}|${MMC_MAIL_REPLICATION_PASSWORD}|g" /container/service/dovecot/assets/config/config-available/replication/config/90-replication.conf
-sed -i "s|{{ MMC_MAIL_REPLICATION_HOST }}|${MMC_MAIL_REPLICATION_HOST}|g" /container/service/dovecot/assets/config/config-available/replication/config/90-replication.conf
+sed -i --follow-symlinks "s|{{ MMC_MAIL_REPLICATION_PASSWORD }}|${MMC_MAIL_REPLICATION_PASSWORD}|g" /container/service/dovecot/assets/config/config-available/replication/config/90-replication.conf
+sed -i --follow-symlinks "s|{{ MMC_MAIL_REPLICATION_HOST }}|${MMC_MAIL_REPLICATION_HOST}|g" /container/service/dovecot/assets/config/config-available/replication/config/90-replication.conf
 
 if [ "${MMC_MAIL_REPLICATION_SSL,,}" == "true" ]; then
-  sed -i "s|#ssl =|ssl =|g" /container/service/dovecot/assets/config/config-available/replication/config/90-replication.conf
-  sed -i "s|tcp:|tcps:|g" /container/service/dovecot/assets/config/config-available/replication/config/90-replication.conf
+  sed -i --follow-symlinks "s|#ssl =|ssl =|g" /container/service/dovecot/assets/config/config-available/replication/config/90-replication.conf
+  sed -i --follow-symlinks "s|tcp:|tcps:|g" /container/service/dovecot/assets/config/config-available/replication/config/90-replication.conf
 fi
 
 exit 0
